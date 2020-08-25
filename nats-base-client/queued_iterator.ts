b764747463520f80827bfa7cb9fc30ae3c264b85
Alberto Ricart <alberto@synadia.com>
2020-08-25 11:09:54 -0500

    added minor optimizations to combat memory allocations, and to remove possible (#43)
    latency if messages are pushed into the iterator while iterating.

--- nats-base-client/queued_iterator.ts
+++ nats-base-client/queued_iterator.ts
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { deferred, Deferred } from "./util.ts";
+import { deferred } from "./util.ts";
 
 export interface Dispatcher<T> {
   push(v: T): void;
@@ -21,8 +21,8 @@ export interface Dispatcher<T> {
 export class QueuedIterator<T> implements Dispatcher<T> {
   processed = 0;
   received = 0; // this is updated by the protocol
-  protected done: boolean = false;
-  private signal: Deferred<void> = deferred<void>();
+  protected done = false;
+  private signal = deferred<void>();
   private yields: T[] = [];
   private err?: Error;
 
@@ -44,15 +44,28 @@ export class QueuedIterator<T> implements Dispatcher<T> {
       if (this.err) {
         throw this.err;
       }
-      while (this.yields.length > 0) {
+      const yields = this.yields;
+      this.yields = [];
+      for (let i = 0; i < yields.length; i++) {
         this.processed++;
-        const v = this.yields.shift();
-        yield v!;
+        yield yields[i];
+      }
+      // yielding could have paused and microtask
+      // could have added messages. Prevent allocations
+      // if possible
+      if (this.yields.length === 0) {
+        yields.length = 0;
+        this.yields = yields;
       }
       if (this.done) {
         break;
       } else {
         this.signal = deferred();
+        // if we were paused push's resolve was noop
+        // self resolve
+        if (this.yields.length) {
+          this.signal.resolve();
+        }
       }
     }
   }
