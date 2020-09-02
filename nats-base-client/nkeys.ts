794ba41bce288519d3397505b5138e84fbf8cdcb
Alberto Ricart <alberto@synadia.com>
2020-09-02 17:23:45 -0500

    bumped nkeys (#55)

--- nats-base-client/nkeys.ts
+++ nats-base-client/nkeys.ts
@@ -1,1 +1,1 @@
-export * as nkeys from "https://raw.githubusercontent.com/nats-io/nkeys.js/v1.0.0-5/modules/esm/mod.ts";
+export * as nkeys from "https://raw.githubusercontent.com/nats-io/nkeys.js/v1.0.0-6/modules/esm/mod.ts";
