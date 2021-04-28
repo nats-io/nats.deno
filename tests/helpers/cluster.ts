8d7402a275327a22f09497e69af2b7a13f6af1fc
Alberto Ricart <alberto@synadia.com>
2021-04-28 10:49:06 -0500

    infra to build js-cluster for tests (#151)
    * [feat] build js-clusters in tests

--- tests/helpers/cluster.ts
+++ tests/helpers/cluster.ts
@@ -15,6 +15,7 @@
 
 import { NatsServer } from "./mod.ts";
 import { parse } from "https://deno.land/std@0.92.0/flags/mod.ts";
+import { rgb24 } from "https://deno.land/std@0.92.0/fmt/colors.ts";
 
 const defaults = {
   c: 2,
@@ -28,27 +29,38 @@ const argv = parse(
       "p": ["port"],
       "c": ["count"],
       "d": ["debug"],
+      "j": ["jetstream"],
     },
     default: defaults,
+    boolean: ["debug", "jetstream"],
   },
 );
 
 if (argv.h || argv.help) {
   console.log(
-    "usage: cluster [--count 2] [--port 4222] [--debug]\n",
+    "usage: cluster [--count 2] [--port 4222] [--debug] [--jetstream]\n",
   );
   Deno.exit(0);
 }
 
 try {
-  const cluster = await NatsServer.cluster(
-    argv.count,
-    { port: argv.port },
-    argv.debug,
-  );
+  const cluster = argv.jetstream
+    ? await NatsServer.jetstreamCluster(
+      argv.count,
+      { port: argv.port },
+      argv.debug,
+    )
+    : await NatsServer.cluster(
+      argv.count,
+      { port: argv.port },
+      argv.debug,
+    );
+
   cluster.forEach((s) => {
+    const pid = `[${s.process.pid}]`;
+    const cpid = rgb24(pid, s.rgb);
     console.log(
-      `launched server [${s.process.pid}] at ${s.hostname}:${s.port} - monitor ${s.monitoring}`,
+      `${cpid} ${s.configFile} at nats://${s.hostname}:${s.port} cluster://${s.hostname}:${s.cluster} http://127.0.0.1:${s.monitoring} - store: ${s.config.jetstream.store_dir}`,
     );
   });
 
