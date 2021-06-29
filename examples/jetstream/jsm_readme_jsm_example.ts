88ec349b1a36599c7ab0859067caef1117a1719d
Alberto Ricart <alberto@synadia.com>
2021-06-29 14:16:47 -0500

    fix jestream readme examples and expose jsm.deleteMessage() `erase` argument (#163)
    - [FIX] jetstream readme examples
    - [FIX] expose jetstreamManagers deleteMessage 'erase' argument
    - [FIX] mark "duplicate_window" as Nanos

---
+++ examples/jetstream/jsm_readme_jsm_example.ts
@@ -0,0 +1,54 @@
+import { AckPolicy, connect, Empty } from "../../src/mod.ts";
+
+const nc = await connect();
+const jsm = await nc.jetstreamManager();
+
+// list all the streams, the `next()` function
+// retrieves a paged result.
+const streams = await jsm.streams.list().next();
+
+// add a stream
+const stream = "mystream";
+const subj = `mystream.A`;
+await jsm.streams.add({ name: stream, subjects: [subj] });
+
+// publish a reg nats message directly to the stream
+for (let i = 0; i < 10; i++) {
+  nc.publish(subj, Empty);
+}
+
+// find a stream that stores a specific subject:
+const name = await jsm.streams.find("mystream.A");
+
+// retrieve info about the stream by its name
+const si = await jsm.streams.info(name);
+
+// update a stream configuration
+si.config.subjects?.push("a.b");
+await jsm.streams.update(si.config);
+
+// get a particular stored message in the stream by sequence
+// this is not associated with a consumer
+let sm = await jsm.streams.getMessage(stream, 1);
+
+// delete the 5th message in the stream, securely erasing it
+await jsm.streams.deleteMessage(stream, 5);
+
+// purge all messages in the stream, the stream itself
+// remains.
+await jsm.streams.purge(stream);
+
+// list all consumers for a stream:
+const consumers = await jsm.consumers.list(stream).next();
+
+// add a new durable pull consumer
+await jsm.consumers.add(stream, {
+  durable_name: "me",
+  ack_policy: AckPolicy.Explicit,
+});
+
+// retrieve a consumer's configuration
+const ci = await jsm.consumers.info(stream, "me");
+
+// delete a particular consumer
+await jsm.consumers.delete(stream, "me");
