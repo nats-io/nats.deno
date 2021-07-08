f0a29e99d52105e9a6ea668b15ee092ff56b389d
Alberto Ricart <alberto@synadia.com>
2021-07-08 17:55:01 -0500

    added poc encoded kv

---
+++ nats-base-client/ekv.ts
@@ -0,0 +1,113 @@
+import { QueuedIterator, QueuedIteratorImpl } from "./queued_iterator.ts";
+import { Empty, PurgeOpts, PurgeResponse } from "./types.ts";
+import { Codec } from "./codec.ts";
+import { Bucket, Entry, KvStatus, PutOptions } from "./kv.ts";
+
+export interface EncodedEntry<T> {
+  bucket: string;
+  key: string;
+  value?: T;
+  created: Date;
+  seq: number;
+  delta?: number;
+  "origin_cluster"?: string;
+  operation: "PUT" | "DEL";
+}
+
+export interface EncodedRoKV<T> {
+  get(k: string): Promise<EncodedEntry<T>>;
+  history(k: string): Promise<QueuedIterator<EncodedEntry<T>>>;
+  watch(opts?: { key?: string }): Promise<QueuedIterator<EncodedEntry<T>>>;
+  close(): Promise<void>;
+  status(): Promise<KvStatus>;
+  keys(): Promise<Set<string>>;
+}
+
+export interface EncodedKv<T> extends EncodedRoKV<T> {
+  put(k: string, data?: T, opts?: Partial<PutOptions>): Promise<number>;
+  delete(k: string): Promise<void>;
+  purge(opts?: PurgeOpts): Promise<PurgeResponse>;
+  destroy(): Promise<boolean>;
+}
+
+export class EncodedBucket<T> implements EncodedKv<T> {
+  bucket: Bucket;
+  codec: Codec<T>;
+  constructor(bucket: Bucket, codec: Codec<T>) {
+    this.bucket = bucket;
+    this.codec = codec;
+  }
+
+  toEncodedEntry(e: Entry): EncodedEntry<T> {
+    return {
+      bucket: this.bucket.bucket,
+      key: e.key,
+      value: e.value.length > 0 ? this.codec.decode(e.value) : undefined,
+      delta: 0,
+      created: e.created,
+      seq: e.seq,
+      origin_cluster: e.origin_cluster,
+      operation: e.operation,
+    };
+  }
+
+  put(
+    k: string,
+    data: T,
+    opts: Partial<PutOptions> = {},
+  ): Promise<number> {
+    const buf = data ? this.codec.encode(data) : Empty;
+    return this.bucket.put(k, buf, opts);
+  }
+
+  async get(k: string): Promise<EncodedEntry<T>> {
+    return this.toEncodedEntry(await this.bucket.get(k));
+  }
+
+  delete(k: string): Promise<void> {
+    return this.bucket.delete(k);
+  }
+
+  async toEncodedIter(
+    src: QueuedIterator<Entry>,
+  ): Promise<QueuedIterator<EncodedEntry<T>>> {
+    const iter = new QueuedIteratorImpl<EncodedEntry<T>>();
+    await (async () => {
+      for await (const e of src) {
+        iter.push(this.toEncodedEntry(e));
+      }
+    })();
+    iter.stop();
+    return iter;
+  }
+
+  async history(k: string): Promise<QueuedIterator<EncodedEntry<T>>> {
+    return this.toEncodedIter(await this.bucket.history(k));
+  }
+
+  async watch(
+    opts: { key?: string } = {},
+  ): Promise<QueuedIterator<EncodedEntry<T>>> {
+    return this.toEncodedIter(await this.bucket.watch(opts));
+  }
+
+  keys(): Promise<Set<string>> {
+    return this.bucket.keys();
+  }
+
+  purge(opts?: PurgeOpts): Promise<PurgeResponse> {
+    return this.bucket.purge(opts);
+  }
+
+  destroy(): Promise<boolean> {
+    return this.bucket.destroy();
+  }
+
+  status(): Promise<KvStatus> {
+    return this.bucket.status();
+  }
+
+  close(): Promise<void> {
+    return this.bucket.close();
+  }
+}
