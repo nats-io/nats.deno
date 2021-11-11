6e42889369c8689d7f7c822ab7b1f1b5d26153a3
Alberto Ricart <alberto@synadia.com>
2021-11-11 15:23:55 -0600

    preparing for release

--- src/deno_transport.ts
+++ src/deno_transport.ts
@@ -32,7 +32,7 @@ import {
 } from "../nats-base-client/internal_mod.ts";
 import type { TlsOptions } from "../nats-base-client/types.ts";
 
-const VERSION = "1.4.0-1";
+const VERSION = "1.4.0";
 const LANG = "nats.deno";
 
 // if trying to simply write to the connection for some reason
