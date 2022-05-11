8b17907fe5cace16fb873a64c6d3cdcd6c352618
Alberto Ricart <alberto@synadia.com>
2022-05-11 15:52:07 -0500

    [FEAT] support inspecting subjects in stream via StreamInfo - ADR issue 91

--- tests/jsm_test.ts
+++ tests/jsm_test.ts
@@ -984,3 +984,27 @@ Deno.test("jsm - update consumer", async () => {
 
   await cleanup(ns, nc);
 });
+
+Deno.test("jsm - stream info subjects", async () => {
+  const { ns, nc } = await setup(jetstreamServerConf({}, true));
+  if (await notCompatible(ns, nc, "2.7.2")) {
+    return;
+  }
+  const jsm = await nc.jetstreamManager();
+  const name = nuid.next();
+  await jsm.streams.add({ name, subjects: [`${name}.>`] });
+
+  const js = nc.jetstream();
+  await js.publish(`${name}.a`);
+  await js.publish(`${name}.a.b`);
+  await js.publish(`${name}.a.b.c`);
+
+  const si = await jsm.streams.info(name, { subjects_filter: `>` });
+  assertEquals(si.state.num_subjects, 3);
+  assert(si.state.subjects);
+  assertEquals(si.state.subjects[`${name}.a`], 1);
+  assertEquals(si.state.subjects[`${name}.a.b`], 1);
+  assertEquals(si.state.subjects[`${name}.a.b.c`], 1);
+
+  await cleanup(ns, nc);
+});
