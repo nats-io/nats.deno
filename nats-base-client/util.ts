fbad17c941f4c81e00c05e5e785c1cbfaf9d275c
Alberto Ricart <alberto@synadia.com>
2022-07-27 12:42:13 -0500

    [DOC] added JSDoc so that the API for the client can be generated and made available (#333)
    [ADD] added models for configuring StreamConfig.republish

--- nats-base-client/util.ts
+++ nats-base-client/util.ts
@@ -112,11 +112,23 @@ export function delay(ms = 0): Promise<void> {
 }
 
 export interface Deferred<T> extends Promise<T> {
+  /**
+   * Resolves the Deferred to a value T
+   * @param value
+   */
   resolve: (value?: T | PromiseLike<T>) => void;
   //@ts-ignore: tsc guard
+  /**
+   * Rejects the Deferred
+   * @param reason
+   */
   reject: (reason?: any) => void;
 }
 
+/**
+ * Returns a Promise that has a resolve/reject methods that can
+ * be used to resolve and defer the Deferred.
+ */
 export function deferred<T>(): Deferred<T> {
   let methods = {};
   const p = new Promise<T>((resolve, reject): void => {
