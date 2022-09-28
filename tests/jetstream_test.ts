a0043e2e6c7c90de2bab3f5e5b90390a3556d61d
Alberto Ricart <alberto@synadia.com>
2022-09-28 15:23:39 -0500

    changed test to resolve once the acks are processed to guarantee the status checks.

--- tests/jetstream_test.ts
+++ tests/jetstream_test.ts
@@ -1490,13 +1490,19 @@ Deno.test("jetstream - cross account subscribe", async () => {
   const js = nc.jetstream({ apiPrefix: "IPA" });
 
   const opts = bo.getOpts();
+  const acks: Promise<boolean>[] = [];
+  const d = deferred();
   const sub = await js.subscribe(subj, opts);
   await (async () => {
     for await (const m of sub) {
-      m.ack();
+      acks.push(m.ackAck());
+      if (m.seq === 2) {
+        d.resolve();
+      }
     }
   })();
-  await nc.flush();
+  await d;
+  await Promise.all(acks);
   const ci = await sub.consumerInfo();
   assertEquals(ci.num_pending, 0);
   assertEquals(ci.delivered.stream_seq, 2);
