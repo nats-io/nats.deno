cfdfc491fc1ffac992844707b330ac8b68466d83
aricart <alberto@synadia.com>
2023-01-12 18:20:05 -0400

    [TEST] fix flappers

--- tests/service_test.ts
+++ tests/service_test.ts
@@ -741,6 +741,7 @@ Deno.test("service - cross platform service test", async () => {
   const p = Deno.run({ cmd: args, stderr: "piped", stdout: "piped" });
   const [status, _stdout, stderr] = await Promise.all([
     p.status(),
+    p.output(),
     p.stderrOutput(),
   ]);
 
