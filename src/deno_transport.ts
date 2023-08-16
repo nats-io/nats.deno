9c21e78a2ec93c74abad4170365c2b2a8a79bf3b
Alberto Ricart <alberto@synadia.com>
2023-08-16 11:40:30 -0500

    bump version for release

--- src/deno_transport.ts
+++ src/deno_transport.ts
@@ -34,7 +34,7 @@ import {
   Transport,
 } from "../nats-base-client/internal_mod.ts";
 
-const VERSION = "1.15.1";
+const VERSION = "1.16.0";
 const LANG = "nats.deno";
 
 const ReadBufferSize = 1024 * 256;
